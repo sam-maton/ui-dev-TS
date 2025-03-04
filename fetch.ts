2941acb84140ab9e98daf8838c32818754b65499
smaton1 <sam.maton91@gmail.com>
2025-03-04 20:23:30 +0000

    setup basic schema validation

--- fetch.ts
+++ fetch.ts
@@ -1,11 +1,28 @@
+import { z } from 'zod'
+
+const apiReturn = z.object({
+  count: z.number(),
+  next: z.string(),
+  results: z.array(
+    z.object({
+      name: z.string(),
+      url: z.string(),
+    })
+  ),
+})
+
 async function getPokemon() {
   const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20'
   const response = await fetch(url)
   const pokemon: unknown = await response.json()
 
-  console.log(pokemon)
-
-  return pokemon
+  return apiReturn.parse(pokemon)
 }
 
 getPokemon()
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((e) => {
+    console.log(e)
+  })
